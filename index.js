f617f2dceb1b1d1d3a1d51216f798c7a787c010b
reyhaneh <reyhanehrm333@gmail.com>
2021-09-18 03:33:47 +0430

    added edit button for tasks

--- index.js
+++ index.js
@@ -48,19 +48,48 @@ const addTodo = (e) => {
     
 }
 
-const deleteTask = (e) => {
+const TaskBtns = (e) => {
+    const button = e.target
 
     if (e.target.classList[0] === 'delete') {
-        const todo = e.target.closest('.task')
+        const todo = button.closest('.task')
         todo.remove()
     }
 
-    else if (e.target.classList[0] === 'complete') {
-        const todo = e.target.closest('.task-content')
+    else if (e.target.classList.contains('complete')) {
+        const todo = button.closest('.task-content')
         todo.classList.toggle('completed')
+    }
+    else if (e.target.classList.contains('edite')) {
+        const todo = button.closest('.task-content').firstChild
+        // console.log(todo , e.target.classList);
+        // console.log(todo);
+        const inputEdit = document.createElement("input")
+        inputEdit.type = 'text'
+        inputEdit.value = todo.textContent
+
+        button.closest('.task-content').insertBefore(inputEdit, todo)
+        todo.remove()
+
+        button.textContent = 'save'
+        button.classList.remove('edite')
+    }
+    
+
+    else if (e.target.textContent === 'save') {
+        const input = e.target.closest('.task-content').firstChild
+         const span = document.createElement('span')
+         span.textContent= input.value
+         button.closest('.task-content').insertBefore(span, input)
+         button.closest('.task-content').removeChild(input)
+         button.textContent = 'edite'
+         button.classList.add('edite')
+
+    
+       
     }
 }
 
 // eventListener
 addBtn.addEventListener('click', addTodo)
-tasksContainer.addEventListener('click' , deleteTask)
+tasksContainer.addEventListener('click' , TaskBtns)
